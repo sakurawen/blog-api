a2fbf2d2cb83e97dea1c4a4e6ac09b53bbd75060
sakurawen <wisakura@outlook.com>
2025-02-16 23:59:58 +0800

    feat: add get blog detail api

--- src/index.ts
+++ src/index.ts
@@ -6,9 +6,15 @@ const app = new Hono<Env>()
 
 app.use(notionMiddleware)
 
-app.get('/', async (c) => {
-  const data = await c.var.notion.getPage(c.env.NOTION_PAGE_ID)
-  return c.json(data)
+app.get('/blog', async (c) => {
+  const result = await c.var.notion.getPage(c.env.NOTION_PAGE_ID)
+  return c.json(result)
+})
+
+app.get("/blog/:id",async (c)=>{
+  const id = c.req.param("id")
+  const result = await c.var.notion.getPage(id)
+  return c.json(result)
 })
 
 export default app
