43c1c1f3593ffa9952d1644761654d5e9b5c1b46
sakurawen <wisakura@outlook.com>
2025-02-17 18:34:42 +0800

    feat: add eslint

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,20 @@
-import { Hono } from 'hono'
-import { Env } from "~/src/type";
-import { notionMiddleware } from "./middleware";
+import type { Env } from '~/src/type';
+import { Hono } from 'hono';
+import { notionMiddleware } from './middleware';
 
-const app = new Hono<Env>()
+const app = new Hono<Env>();
 
-app.use(notionMiddleware)
+app.use(notionMiddleware);
 
 app.get('/blog', async (c) => {
-  const result = await c.var.notion.getPage(c.env.NOTION_PAGE_ID)
-  return c.json(result)
-})
+  const result = await c.var.notion.getPage(c.env.NOTION_PAGE_ID);
+  return c.json(result);
+});
 
-app.get("/blog/:id",async (c)=>{
-  const id = c.req.param("id")
-  const result = await c.var.notion.getPage(id)
-  return c.json(result)
-})
+app.get('/blog/:id', async (c) => {
+  const id = c.req.param('id');
+  const result = await c.var.notion.getPage(id);
+  return c.json(result);
+});
 
-export default app
+export default app;
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,11 +1,12 @@
-import { NotionAPI } from "notion-client";
-import {createFactory} from "hono/factory";
-import {Env} from "~/src/type";
+import type { Env } from '~/src/type';
+import { createFactory } from 'hono/factory';
+import { NotionAPI } from 'notion-client';
+
 const factory = createFactory<Env>();
 
-export const notionMiddleware = factory.createMiddleware((c,next)=>{
-  c.set("notion",new NotionAPI({
-    authToken:c.env.NOTION_KEY
-  }))
-  return next()
-})
+export const notionMiddleware = factory.createMiddleware((c, next) => {
+  c.set('notion', new NotionAPI({
+    authToken: c.env.NOTION_KEY,
+  }));
+  return next();
+});
